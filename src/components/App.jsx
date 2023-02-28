9882e6225a468c97949f359404c5fe1d8ce41ce8
Oleksandr Deoneha <oleksandr.deoneha@gmail.com>
2023-02-28 02:15:04 +0200

    initial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
+import { ContactForm } from './Form/Form';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import { useSelector } from 'react-redux';
+import { getStatusFilter, getContacts } from 'redux/selectors';
+
+export default function App() {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getStatusFilter);
+
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 20,
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      {contacts.length || filter ? (
+        <>
+          <Filter />
+          <ContactList />
+        </>
+      ) : (
+        <p>Contacts is empty!</p>
+      )}
     </div>
   );
-};
+}
---
+++ src/redux/selectors.js
@@ -0,0 +1,2 @@
+export const getStatusFilter = state => state.filter;
+export const getContacts = state => state.contacts;
